a72d5ec57595f1d538d35ec475505dd940942f11
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-01-13 11:02:34 +0100

    fix config file

--- src/config/google.config.ts
+++ src/config/google.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('google', () => ({
 	google: {
-		privateKey: Buffer.from(process.env.GOOGLE_PRIVATE_KEY || '', 'base64').toString('ascii'),
+		privateKey: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
 		clientEmail: process.env.CLIENT_EMAIL,
 	},
 }));
